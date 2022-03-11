a4056fa3e6f4201fcf105f40a70d188c7be71c4d
salamander202 <tv30254@gotvdsb.ca>
2022-03-10 18:05:14 -0800

    added even more text

--- src/textAdventure/Game.java
+++ src/textAdventure/Game.java
@@ -5,6 +5,7 @@ public class Game {
 	public static void main(String[] args) {
 		System.out.println("Welcome to my text adventure game");
 		System.out.println("wow ");
+		System.out.println("I hope you enjoyed this.");
 	}
 
 }
